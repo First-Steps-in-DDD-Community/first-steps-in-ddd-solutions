0c6a0ece1f72cde8fa8d55ca3e7f89300daf914e
Andrew Harmel-Law <andrew@harmel-law.com>
2020-06-26 12:46:56 +0100

    Exercise 1 - REFACTOR - Use a test fixture.

--- src/test/java/com/harmellaw/APreChargeDecision.java
+++ src/test/java/com/harmellaw/APreChargeDecision.java
@@ -7,16 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class APreChargeDecision {
 
+    private Suspect suspect;
+    private PreChargeDecision aPreChargeDecision;
+    private OffenceAdvice offenceAdvice;
+
     @BeforeEach
     public void setup() {
+        suspect = new Suspect(CriminalOffence.CUTTING_AWAY_BUOYS_ETC);
+        aPreChargeDecision = new PreChargeDecision();
+        offenceAdvice = new OffenceAdvice();
     }
 
     @Test
     public void shouldRecordAlternativeOffenceAdviceAgainstSuspects() {
-        Suspect suspect = new Suspect(CriminalOffence.CUTTING_AWAY_BUOYS_ETC);
-        PreChargeDecision aPreChargeDecision = new PreChargeDecision();
-        OffenceAdvice offenceAdvice = new OffenceAdvice();
-
         aPreChargeDecision.recordAlternativeOffenceAdvice(suspect, offenceAdvice);
 
         assertEquals(offenceAdvice, aPreChargeDecision.getOffenceAdviceFor(suspect));
