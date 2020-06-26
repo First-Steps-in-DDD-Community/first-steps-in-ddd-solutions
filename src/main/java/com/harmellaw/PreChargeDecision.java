eae5b0af0ae1c188ba4914389fb9d74c9d5ea3c3
Andrew Harmel-Law <andrew@harmel-law.com>
2020-06-26 12:42:21 +0100

    Exercise 1 - Implement missing class and functions - tests still fail.

--- src/main/java/com/harmellaw/PreChargeDecision.java
+++ src/main/java/com/harmellaw/PreChargeDecision.java
@@ -1,4 +1,18 @@
 package com.harmellaw;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PreChargeDecision {
+
+    private Map<Suspect, OffenceAdvice> offenceAdvice = new HashMap<>();
+
+    public void recordAlternativeOffenceAdvice(Suspect suspect, OffenceAdvice advice) {
+
+    }
+
+    public OffenceAdvice getOffenceAdviceFor(Suspect suspect) {
+        return null;
+    }
+
 }
