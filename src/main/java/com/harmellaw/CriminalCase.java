ffc044c8d714d3ba091a5130164f0734014e9b96
Andrew Harmel-Law <andrew@harmel-law.com>
2020-06-26 13:06:05 +0100

    Exercise 2 - GREEN - Add PublicProsecutionService class and attributes to CriminalCase - Test passes

--- src/main/java/com/harmellaw/CriminalCase.java
+++ src/main/java/com/harmellaw/CriminalCase.java
@@ -1,4 +1,15 @@
 package com.harmellaw;
 
+import java.util.Set;
+
 public class CriminalCase {
+
+    public PNCId pncId;
+    public Set<Suspect> suspects;
+
+    public CriminalCase(PNCId pncId, Set<Suspect> suspects) {
+        this.pncId = pncId;
+        this.suspects = suspects;
+    }
+
 }
